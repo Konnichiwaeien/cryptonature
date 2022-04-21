d531b3acb76036b6eadc66eaaae00254d5f394db
konnichiwaeien <konnichiwaeien@gmail.com>
2022-04-21 14:46:26 +0500

    feat: add redux provider and store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/App/App';
+import store from './store/store';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
